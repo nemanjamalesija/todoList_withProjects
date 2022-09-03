88c608bad5e4b3b36abcc696158e8733b1b29b9c
Nemanja Malesija <malesija13@gmail.com>
2022-09-04 00:51:02 +0200

    add todo, project, projectManager class

---
+++ script.js
@@ -0,0 +1,40 @@
+// todos
+class toDo {
+  id = self.crypto.randomUUID();
+  constructor(title, dueDate) {
+    (this.title = title), (this.dueDate = dueDate);
+  }
+}
+
+// project
+class Project {
+  id = self.crypto.randomUUID();
+  todos = [];
+  constructor(projectTitle) {
+    this.projectTitle = projectTitle;
+  }
+
+  pushTodo(todo) {
+    this.todos.push(todo);
+    return [...this.todos];
+  }
+}
+
+// project manager
+class ProjectManager {
+  projects = [];
+  clickedProject;
+  clickedProjectId;
+  addTodo;
+  addTodoEdited;
+  clickedTodoId;
+
+  constructor() {
+    ///// EVENT LISTENERS
+  }
+
+  ///// METHODS
+  addProject(pTitle) {
+    this.projects.push(pTitle);
+  }
+}
